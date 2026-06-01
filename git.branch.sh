db1af9352dae0b36df6f6a9e17fcd7c9396d4623
Addison <addison@pixelpetals.ca>
2026-05-31 21:43:29 -0400

    git branch

---
+++ git.branch.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+function git.branch.fromRemote () {
+	local origin
+	local repo
+	local branch=
+
+	origin="$1"
+	branch="${2:-HEAD}"
+
+	repo=$(basename "$origin")
+	repo_name="${repo%.*}"
+
+	git checkout --orphan "remote/$repo_name"
+	git rm -rf .
+	git commit --allow-empty -m "init"
+	git pull "$origin" "$branch"
+
+}
