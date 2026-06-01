8e0cc0c082e793172bf4e4b83b9d5d7fdf344784
Addison <addison@pixelpetals.ca>
2026-05-31 22:15:32 -0400

    update git.branch.sh

--- git.branch.sh
+++ git.branch.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+function git.origin () {
+	git remote get-url origin
+}
+
 function git.branch.fromRemote () {
 
 	local origin
@@ -12,10 +16,30 @@ function git.branch.fromRemote () {
 	repo=$(basename "$origin")
 	repo_name="${repo%.*}"
 
-	git checkout --orphan "remote/$repo_name"
+	local subtree
+	subtree="remote/$repo_name"
+
+	git checkout --orphan "$subtree"
 	git rm -rf .
 	git commit --allow-empty -m "init"
 	git pull "$origin" "$branch"
 	git push -u origin
+
+	git checkout main
+	git submodule add -b "$subtree" "$origin" "$subtree"
+
+}
+
+function git.submodule.self () {
+
+	local origin
+	local branch
+	local path
+
+	origin=$(git.origin)
+	branch="$1"
+	path="${2:-$1}"
+
+	git submodule add -b "$branch" "$origin" "$path"
 
 }
